f0749cd6157e43e049009fdde299a550b333d03a
Chathura <divinecorps@gmail.com>
2025-03-15 21:11:25 +0100

    add auth

--- hotelapp/src/App.js
+++ hotelapp/src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <button onClick={() => {window.location.href="/auth/login"}}>Login</button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
